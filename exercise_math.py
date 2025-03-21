abd894414cddb63614844558ca0a072f50ee0c48
BautiValles <150041744+BautiValles@users.noreply.github.com>
2025-03-21 12:02:49 -0300

    Update exercise_math.py

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,17 @@
 def math():
     a = 57
     b = 7
+    suma = a+b
+    resta = a-b
+    producto = a*b
+    promedio = (a+b)/2
+    div_entero = int(a/b)
+    resto = a%b
+    div_decim = a/b
+    print(suma)
+    print(resta)
+    print(producto)
+    print(promedio)
+    print(div_entero)
+    print(resto)
+    print(div_decim)
